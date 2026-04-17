381b4ab6e6a80efcee7626e5c09a2c7f3593b90f
Hansel Solis <hanseldavidsolismarinxd11.3@gmail.com>
2026-04-17 14:27:58 -0500

    Add files via upload

---
+++ MainJavaEs/src/Main.java
@@ -0,0 +1,59 @@
+import javax.sound.sampled.*;
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.util.Scanner;
+
+public class Main {
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        //How to play audio with Java ( .wav, .au, aiff)
+
+        String filePath = "src\\Me_enamore_de_la_beach.wav"; //This is the name of song
+        File file = new File(filePath);
+
+        //The use of "Try" is important to avoid mistakes while someone want to reproduce a song
+        try(AudioInputStream audioInputStream = AudioSystem.getAudioInputStream(file);){
+
+            Clip clip = AudioSystem.getClip();
+            clip.open(audioInputStream);
+
+            String response = " ";
+            while (!response.equals("Q")){
+
+                System.out.println("P = Play");
+                System.out.println("S = Stop");
+                System.out.println("R = Reset ");
+                System.out.println("Q = Quit");
+                System.out.println("Choice: ");
+                response = scanner.next().toUpperCase();
+
+                switch (response){
+                    case "P" -> clip.start();
+                    case "S" -> clip.stop();
+                    case "R" -> clip.setMicrosecondPosition(0);
+                    case "Q" -> clip.close();
+                    default -> System.out.println("Invalid choice");
+                }
+            }
+        }
+        catch (FileNotFoundException e){
+            System.out.println("Could not locate file");
+        }
+        catch(UnsupportedAudioFileException e){
+            System.out.println("Audio file is not supported");
+        }
+        catch(LineUnavailableException e){
+            System.out.println("Unable to access audio resource");
+        }
+        catch (IOException e){
+            System.out.println("Something went wrong");
+        }
+        finally {
+            System.out.println("Thank for reproducing the song, see you!");
+            scanner.close();
+        }
+    }
+}
+
